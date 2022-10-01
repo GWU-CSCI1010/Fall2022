e4e54ef186a0548f226eb49367da1f34e66379a8
Josie Libbon <32231022+josielibbon@users.noreply.github.com>
2022-10-01 11:57:16 -0400

    Update LED_1.py

--- Week1/LED_1.py
+++ Week1/LED_1.py
@@ -3,7 +3,7 @@
 ==== MAE Department, SEAS GWU
 ==== Description
 ======== This program is for blinking an LED wired to Raspberry PI
-======== It has been written exclusively for CS1010 & APSC1001 students in GWU.
+======== It has been written exclusively for CS1010 students in GWU.
 ======== It may not be used for any other purposes unless author's prior permission is aquired.
 """
 
@@ -12,13 +12,18 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup([11,12], GPIO.OUT)
 
-for i in range(0,15):
+for i in range(0,5):
     
     GPIO.output(12, GPIO.HIGH)
     time.sleep(0.5)
     GPIO.output(12, GPIO.LOW)
+    time.sleep(0.5)
+    
+    GPIO.output(11, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(11, GPIO.LOW)
     time.sleep(0.5)
     print(i)
     
